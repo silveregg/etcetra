3fa2f3ba899d0cc633aee1bf7468ca66d3a6cd93
Zhuravlev Yury <yury@silveregg.co.jp>
2025-06-17 12:07:49 +0900

    bump deps and fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,25 @@
 import os
 import pytest
+import pytest_asyncio
 
-from etcetra import EtcdClient, HostPortPair
+from etcetra import EtcdClient, EtcdCredential, HostPortPair
 
 
 @pytest.fixture
 def etcd_addr():
     env_addr = os.environ.get('BACKEND_ETCD_ADDR')
-    if env_addr is not None:
+    if env_addr:
         return HostPortPair.parse(env_addr)
     return HostPortPair.parse('localhost:2379')
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def etcd(etcd_addr):
-    etcd = EtcdClient(etcd_addr)
+    creds = EtcdCredential(
+        username=os.environ.get('BACKEND_ETCD_USERNAME', 'root'),
+        password=os.environ.get('BACKEND_ETCD_PASSWORD', 'root')
+    )
+    etcd = EtcdClient(etcd_addr, creds)
     try:
         yield etcd
     finally:
